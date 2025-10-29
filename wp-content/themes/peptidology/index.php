03316374d439349479a86fa00907dcae7f265e30
Ryan <ryancaigoy@gmail.com>
2025-10-28 21:06:20 -0700

    Adds blank theme peptidology2 for change base

---
+++ wp-content/themes/peptidology/index.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Peptidology
+ */
+
+get_header();
+?>
+
+<div class="products-crd-sec cmn-gap">
+    <div class="container">
+        <div class="row products-crd-row">
+		<?php
+		if ( have_posts() ) :
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
+				get_template_part( 'template-parts/content', get_post_type() );
+			endwhile;
+			the_posts_navigation();
+		else :
+			get_template_part( 'template-parts/content', 'none' );
+		endif;
+		?>
+		</div>
+	</div>
+</div>
+
+
+<?php
+get_footer();
